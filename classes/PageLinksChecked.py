3b56daaf0f3d8612d554cdc218550ece04c1e6d1
Predragf <predragku@gmail.com>
2015-03-30 15:40:40 +0200

    Dictionary cache added for optimization

--- classes/PageLinksChecked.py
+++ classes/PageLinksChecked.py
@@ -13,15 +13,27 @@ class PageLinksChecked():
 			#999 is default value for status of the link
 			self.links.append(PageLinkResult(lnk, 999))
 
-	def CheckLinks(self):
+	def CheckLinks(self, _cache):
 		"""Function that iterates though all the links extracted from the document and checks the status. """
 		for lnk in self.links:
 			try:
-				response = urllib2.urlopen(lnk.linkName)
-				lnk.statusCode = response.code
+				#here cache first needs to be implemented. Implement hash using dictionary
+				rCode = _cache.get(lnk.linkName)
+				if rCode is None:
+					print 'cache miss'
+					rCode = urllib2.urlopen(lnk.linkName).code
+					_cache[lnk.linkName] = rCode
+				else:
+					print 'cache hit'
+
+				lnk.statusCode = rCode					
 				if(str(lnk.statusCode).startswith('4') or str(lnk.statusCode).startswith('5')):
 					self.totalInvalidLinks = self.totalInvalidLinks + 1
+			except urllib2.URLError:
+				print 'timeout {0}'.format(lnk.linkName)
+				_cache[lnk.linkName] = '408'
 			except:
 				self.totalInvalidLinks = self.totalInvalidLinks + 1
+				
 
 		
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os.path
 
 
 def main():
+	_cache = {}
 	explorer = DirExplorer()
 	#put the root directory where the files need to be checked
 	rootDir = os.getcwd()
@@ -15,12 +16,15 @@ def main():
 		print fileForProcess
 		parseRes = FileParser.ParseFile(fileForProcess)
 		pagesLinksChecked = PageLinksChecked(parseRes)
-		pagesLinksChecked.CheckLinks()
+		pagesLinksChecked.CheckLinks(_cache)
+		print 'second call'
+		pagesLinksChecked.CheckLinks(_cache)
 		for lnk in pagesLinksChecked.links:
 			print lnk.linkName
 			print lnk.statusCode
 			print '------------------------'
 		print 'total links {0} and links not working {1}'.format(len(pagesLinksChecked.links), pagesLinksChecked.totalInvalidLinks)
+		print _cache
 
 
 main()
